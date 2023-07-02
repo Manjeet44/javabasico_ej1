774a8fdb740e3a863b682db698342978e66ec0ad
Manjeet44 <andrew.vab@gmail.com>
2023-07-02 20:34:01 +0200

    ej1

---
+++ src/com/javabsicoej1/Main.java
@@ -0,0 +1,29 @@
+package com.javabsicoej1;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    /**
+     * Para este primer ejercicio tendréis que realizar lo siguiente:
+     *
+     * Crea un proyecto de Java desde 0
+     *
+     * Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+     *
+     * Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+     *
+     * Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+     */
+    public static void main(String[] args) {
+        int numero = 4;
+        System.out.println(numero);
+        long otroNum = 104;
+        System.out.println(otroNum);
+        double tercerNum = 11.4d;
+        System.out.println(tercerNum);
+        boolean esVerdad = true;
+        System.out.println(esVerdad);
+        String mensaje = "Esto es una cadena de texto";
+        System.out.println(mensaje);
+    }
+}
